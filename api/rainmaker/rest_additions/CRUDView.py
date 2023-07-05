cb158464819f5bed9bb09fd637c13cf9fa763335
Michal Jan Warecki <mwarecki1@gmail.com>
2023-07-05 22:00:04 +0200

    List View

--- api/rainmaker/rest_additions/CRUDView.py
+++ api/rainmaker/rest_additions/CRUDView.py
@@ -15,7 +15,7 @@ class CRUDView(View):
     model the user is trying to get.
     """
     
-    model = ... 
+    model = ...
     """
     @var ModelBase - model this view refers to
     """
@@ -75,3 +75,69 @@ class CRUDView(View):
 
     def delete(self, request, *args, **kwargs):
         return unimplemented()
+
+
+class ListView(View):
+    """
+    If method is GET, set self.instances to the list of the instances of the
+    model the user is trying to get.
+    """
+    
+    model = ...
+    """
+    @var ModelBase - model this view refers to
+    """
+
+    instances = ...
+    """
+    @var list[ModelBase] - instance of the model
+    """
+
+    identifiers: list = ...
+    """
+    @var list[list[tuple]] - list of tuples with URL query variable and model field name
+    """
+
+    links: Union[dict, None] = None
+    """
+    @var dict - links to include in the *_links* field in the HAL response
+    """
+
+    def setup(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        super().setup(request, *args, **kwargs)
+
+        filter_query = None
+        for query in self.identifiers:
+            for identifier in query:
+                partial_query = {}
+
+                if (isinstance(identifier, tuple)):
+                    (url_identifier, model_identifier) = identifier
+                else:
+                    url_identifier = identifier
+                    model_identifier = identifier
+                
+                if url_identifier in kwargs.keys():
+                    partial_query[model_identifier] = kwargs[url_identifier]
+                
+                if partial_query:
+                    filter_query = (filter_query | Q(**partial_query)) \
+                        if filter_query is not None else Q(**partial_query)
+        
+        try:
+            self.instances = self.model.objects.filter(filter_query)
+        except Exception as e:
+            return JsonResponse({"original_exception": repr(e)}, status=404)
+    
+    def get(self, request, *args, **kwargs):
+        response = {
+            'items': [
+                x.serialize() for x in self.instances
+            ]
+        }
+
+        if self.links is not None:
+            response["_links"] = self.links
+        
+        return JsonResponse(response)
+
